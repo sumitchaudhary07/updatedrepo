d22e425977f55e2bd52d67f0ad4189f651057931
PRIYA <sumitchaudhary07@gmail.com>
2024-06-19 01:03:37 +0530

    base chnages

--- src/test/java/sumitframework/basepackage/Base.java
+++ src/test/java/sumitframework/basepackage/Base.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -32,10 +33,17 @@ public class Base {
 			FileInputStream fis=new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\java\\resource\\Global.properties");
 		 Properties p=new Properties();
 		 p.load(fis);
+	String browserName=	 System.getProperty("browser")!=null?System.getProperty("browser"):p.getProperty("browser");
 		System.out.println(p.getProperty("browser")); 
 		 
-		 
+		 if(browserName.equalsIgnoreCase("chrome"))
+		 {
 	 driver =new ChromeDriver();
+		 }
+		 else if(browserName.equalsIgnoreCase("firefox"))
+		 {
+			 driver =new FirefoxDriver();
+		 }
 	
 	 
 	return driver;
